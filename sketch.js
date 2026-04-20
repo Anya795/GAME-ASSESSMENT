7480d8e43b9110fad461fcb58f7a7b0ad2322690
Anya795 <psd540@york.ac.uk>
2026-04-20 17:35:22 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
+let libaryImg;
+
 function setup() {
   createCanvas(400, 400);
+  image(libaryImg, 0,0)
 }
 
 function draw() {
   background(220);
 }
-function setup(){
-  
+
+function preload (){
+  libaryImg = loadImage('/assets/demo.png')
 }
