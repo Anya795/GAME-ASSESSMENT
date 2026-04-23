a0dfac2cec4767c21d872fa75e05bb070a1176f3
Anya795 <psd540@york.ac.uk>
2026-04-23 11:31:10 +0100

    hi

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 let libaryImg;
 let libaryBackgroundImg;
+let spriteImg;
 
 function setup() {
   createCanvas(400, 400);
   image(libaryImg, 0,0);
   image(libaryBackgroundImg, 0,0);
+  image(spriteImg, 0, 0);
 }
 
 function draw() {
@@ -13,6 +15,7 @@ function draw() {
  function preload (){
   libaryImg = loadImage('/assets/Demo.png');
   libaryBackgroundImg = loadImage('/assets/Background.png');
+  spriteImg = loadImage('/assets/prototype_character.png')
  }
 
 
